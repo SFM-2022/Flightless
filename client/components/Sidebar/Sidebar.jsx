a01a6d396482f25c9da3b6fb99a959fc638c1120
neighbor-peace <mcostello91@gmail.com>
2022-11-05 09:26:00 -0700

    ad Navlinks to Sidebar

--- client/components/Sidebar/Sidebar.jsx
+++ client/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Sidebar() {
-  return <h1>Sidebar</h1>;
+  return (
+    <nav>
+      <h1>Sidebar</h1>
+      <NavLink to='/'>Search Results</NavLink>
+      <NavLink to='/saved-trips'>Saved Trips</NavLink>
+    </nav>
+  );
 }
