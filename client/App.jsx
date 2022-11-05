4fcc2b9181b1c210e2fbb07845fb33e2ea8b139f
neighbor-peace <mcostello91@gmail.com>
2022-11-05 09:22:30 -0700

    init App, Navbar, Sidebar, NotFound, SavedTrips, SearchResults

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import SearchResults from './pages/SearchResults/SearchResults.jsx';
+import SavedTrips from './pages/SavedTrips/SavedTrips.jsx';
+import NotFound from './pages/NotFound/NotFound.jsx';
 
 export default function App() {
+  const [formData, setFormData] = useState({
+    depLocation: '',
+    arrLocation: '',
+    depDate: '',
+    numAdults: 0,
+    numChildren: 0,
+    numInfants: 0,
+    cabinClass: '',
+    roundTrip: false,
+  });
   useEffect(() => {
     axios
       .get('/api/test')
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Sidebar />
+        <Routes>
+          <Route
+            path='/'
+            element={<SearchResults />}
+          />
+          <Route
+            path='/saved-trips'
+            element={<SavedTrips />}
+          />
+          <Route
+            path='*'
+            element={<NotFound />}
+          />
+        </Routes>
+      </div>
+    </>
+  );
 }
---
+++ client/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Sidebar() {
+  return <h1>Sidebar</h1>;
+}
