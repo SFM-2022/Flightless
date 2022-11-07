edc948543a3f84cb92550bdb947d30aca80feebc
ks1009 <karenshi1009@gmail.com>
2022-11-06 20:01:09 -0500

    updated new form

--- client/components/Sidebar/Sidebar.jsx
+++ client/components/Sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ export default function Sidebar() {
   return (
     <nav>
       <h1>Sidebar</h1>
-      <NavLink to='/'>Search Results</NavLink>
+      <NavLink to='/'>Search Results </NavLink>
       <NavLink to='/saved-trips'>Saved Trips</NavLink>
     </nav>
   );
