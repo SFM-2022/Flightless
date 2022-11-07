69703b215adf4441d36cd3e58d018540aae80414
neighbor-peace <mcostello91@gmail.com>
2022-11-06 20:01:18 -0800

    init flightController.js

--- server/controllers/flightController.js
+++ server/controllers/flightController.js
@@ -1,3 +1,46 @@
 const flightController = {};
 
+// use dummy values for testing if possible so we don't go over the freemium limit
+const dummyOnewayTripRes = require('../testingData/onewayTripRes');
+const dummyRoundTripRes = require('../testingData/roundTripRes');
+const useDummyData = true;
+
+flightController.fetchData = (req, res, next) => {
+  if (useDummyData) {
+    console.log('flightController.fetchData req.body value: ', req.body);
+    const dummyData = req.body.roundTrip
+      ? dummyRoundTripRes
+      : dummyOnewayTripRes;
+    res.locals.flights = dummyData;
+    return next();
+  } else {
+    // TODO: write middleware to format call to API
+  }
+};
+
+flightController.formatData = (req, res, next) => {
+  // return an array of flights
+  const formattedData = [];
+  /*
+  flight object template
+  {
+    id: STRING, // can use for making React keys
+    price: INTEGER, // total price in USD, rounded to nearest dollar
+    provider: STRING, // booking website we will redirect to
+    handoffUrl: STRING, // url of booking website
+    legs: ARRAY, // contains leg objects one leg for oneway, two for round trip
+  }
+
+  leg object template
+  {
+    id: STRING,
+    departureTime: STRING,
+    arrivalTime: STRING,
+    duration: STRING,
+    airlines: ARRAY, // elements are names of each airline involved
+    stopovers: ARRAY, // elements are names of each stopover airport
+  }
+  */
+};
+
 module.exports = flightController;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const tripController = require('./controllers/tripController.js');
+const flightController = require('./controllers/flightController.js');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,18 +12,18 @@ app.get('/api/test', (req, res) => {
 });
 
 // Route returns all trips user has saved
-// TODO: add middleware to fetch favorited rows
+// TODO: add middleware to fetch ALL rows
 app.get('/api/trips', (req, res) => {
   return res.status(200).json(res.locals.trips);
 });
 
 // Route adds row to trips table
-// TODO: add middleware to add row
 app.post('/api/trips', tripController.createData, (req, res) => {
-  console.log('sending 200 from POST to /api/trips')
+  console.log('sending 200 from POST to /api/trips');
   return res.status(200).send();
 });
 
+/* STRETCH FEATURES
 // Route edits row in table
 // TODO: add middleware to update row
 app.patch('/api/trips', (req, res) => {
@@ -34,10 +35,11 @@ app.patch('/api/trips', (req, res) => {
 app.delete('/api/trips', (req, res) => {
   return res.status(200).send();
 });
+*/
 
 // Route fetches data from API and returns to front end
 // TODO: add middleware to fetch from API
-app.get('/api/flights', (req, res) => {
+app.get('/api/flights', flightController.fetchData, (req, res) => {
   return res.status(200).send(res.locals.flights);
 });
 
